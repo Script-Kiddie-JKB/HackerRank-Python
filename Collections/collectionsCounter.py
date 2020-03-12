c1fd83877c4051a05385bad00ae43accbc00e94e
phoenixx1 <xphoenix1010@gmail.com>
2020-03-13 00:41:10 +0530

    Initial commit

--- Collections/collectionsCounter.py
+++ Collections/collectionsCounter.py
@@ -1,12 +1,15 @@
 #author @Nishant
+from collections import Counter
 
 x = int(input())
-shoe_size = list(map(int,input().split()))
+
+sSize = Counter(map(int, input().split()))
+
 n = int(input())
-sell = 0
-for i in range(n):
-    s, p = map(int,input().split())
-    if s in shoe_size:
-        sell = sell + p
-        shoe_size.remove(s)
-print(sell)
+earning = 0
+for i in range(0,n):
+    sWant, price = map(int, input().split())
+    if(sSize[sWant]!=0):
+        sSize[sWant] -= 1
+        earning += price
+print(earning)
