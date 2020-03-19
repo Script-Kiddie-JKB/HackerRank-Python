dbb2d1c6c3b00a3c905a24332c8b80a615e7d37a
phoenixx1 <xphoenix1010@gmail.com>
2020-03-20 00:00:32 +0530

    Initial commit

--- Math/Triangle Quest.py
+++ Math/Triangle Quest.py
@@ -1,4 +1,5 @@
 #author @Nishant
 
 for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
-    print((10**(i)//9)*i)
+    #print((10**(i)//9)*i)
+    print(str(i)*i)
